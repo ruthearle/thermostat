e3def2b40eb89f72a65fcdce753e2c3e153fb5d0
Roi Driscoll <roi.driscoll@gmail.com>
2014-09-18 00:48:29 +0100

    Increase and Decrease Temp functions created

--- spec/ThermostatSpec.js
+++ spec/ThermostatSpec.js
@@ -9,4 +9,30 @@ describe("Thermostat", function() {
     expect(thermostat.temperature).toEqual(20)
   });
 
+  describe("Increasing the temperature", function() {
+    it('increases if < 35 degrees', function(){
+      thermostat.increaseTemperature()
+      expect(thermostat.temperature).toEqual(21);
+    });
+
+    it('will not increase if temperature is >= 25 degrees', function(){
+      thermostat.temperature = 25;
+      thermostat.increaseTemperature()
+      expect(thermostat.temperature).toEqual(25);
+    })
+  });
+
+  describe("Decreasing the temperature", function() {
+    it('decreases if > 10 degrees', function(){
+      thermostat.decreaseTemperature()
+      expect(thermostat.temperature).toEqual(19);
+    });
+
+    it('will not decrease if temperature is <= 10 degrees', function(){
+      thermostat.temperature = 10;
+      thermostat.decreaseTemperature()
+      expect(thermostat.temperature).toEqual(10);
+    })
+  });
+
 });
---
+++ src/Thermostat.js
@@ -0,0 +1,11 @@
+function Thermostat() {
+  this.temperature = 20;
+}
+
+Thermostat.prototype.increaseTemperature = function() {
+  if(this.temperature < 25) this.temperature += 1;
+};
+
+Thermostat.prototype.decreaseTemperature = function() {
+  if(this.temperature > 10) this.temperature -= 1;
+};
